9d5024e57fc6973b90efb9e8bd2cb4af9ac27fda
WahidinAji <a17wahidin@gmail.com>
2021-06-03 09:54:20 +0700

    delete enter

--- nodejs-basic/process-object/index.js
+++ nodejs-basic/process-object/index.js
@@ -1,6 +1,5 @@
 const initialMemoryUsage = process.memoryUsage().heapUsed;
 const yourName = process.argv[2];
-
 const environment = process.env.NODE_ENV !== 'production' ? 'development' : 'aji.wahidin';
 
 for(let i = 0; i <= 10000; i++){
