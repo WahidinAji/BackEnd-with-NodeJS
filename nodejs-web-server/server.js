070a74e68ad519b8b7f76f8a3ea7fc3e4bbab0da
WahidinAji <a17wahidin@gmail.com>
2021-06-04 14:39:54 +0700

    practice with method and destructuring method

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,16 +1,33 @@
-console.log("'Hello, we'll ready to build server");
 const http = require('http');
 
-const requestListener = (request, response)=>{
-    response.setHeader('Content-Type','text/html');
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+
+    const { method } = request;
+
+    if(method === 'GET') {
+        response.end('<h1>Hello! GET</h1>');
+    }
+
+    if(method === 'POST') {
+        response.end('<h1>Hai! POST</h2>');
+    }
+
+    if(method === 'PUT') {
+        response.end('<h1>Bonjour! PUT</h1>');
+    }
+
+    if(method === 'DELETE') {
+        response.end('<h1>Salam! DELETE</h1>');
+    }
 };
 
 const server = http.createServer(requestListener);
+
 const port = 5000;
 const host = 'localhost';
 
-server.listen(port,host, () => {
+server.listen(port, host, () => {
     console.log(`Server berjalan pada http://${host}:${port}`);
 });
