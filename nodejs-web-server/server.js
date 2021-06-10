91a561a626c1b28fba4a30ebfc5b190dd8d6c962
WahidinAji <a17wahidin@gmail.com>
2021-06-10 14:10:28 +0700

    changed content-type to json

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    // response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
 
     const { method, url } = request;
 
